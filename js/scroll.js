7f6cc2468951e064b21d17395756268bc8712599
VincentiSean <vincenti.sean@gmail.com>
2020-09-16 12:34:19 -0400

    Various support changes and comments added

--- js/scroll.js
+++ js/scroll.js
@@ -5,6 +5,8 @@ var linkDiv = document.getElementById('navlinks-div');
 window.addEventListener('scroll', function ( event ) {
     let scrollNum = document.querySelector('html').scrollTop;
     
+    // If user is at the top of the page, bring top content down
+    // if not, put top content in corners
     if (scrollNum === 0 ) {
         nameText.style.transform = 'translate(0px, 0px)';
         linkDiv.style.transform = 'translate(0px, 0px)';
